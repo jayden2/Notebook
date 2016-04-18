5ffdc86d430ad6b272b55f0f5975b83a61da0cb8
Jayden Spencer <jaydenspencer1@gmail.com>
2016-04-18 23:55:52 +0800

    added simple array adapter

--- app/src/main/java/com/jayden/notebook/MainActivityListFragment.java
+++ app/src/main/java/com/jayden/notebook/MainActivityListFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 /**
@@ -15,6 +16,14 @@ public class MainActivityListFragment extends ListFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        String[] values = new String[] { "Example 1", "Example 2", "Example 3", "Example 4",
+                "Example 5", "Example 6", "Example 7", "Example 8" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+
+        setListAdapter(adapter);
     }
 
     @Override
