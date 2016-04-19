26b41591c34d2042340836b6dbd21b10b526704f
Jayden Spencer <jaydenspencer1@gmail.com>
2016-04-20 02:07:11 +0800

    Added NoteViewFrament get and set, FragmentMananger
    FragmentMananger to display contents of a note
    
    also added back button to parent main activity
    also set title of NoteViewFragment

--- app/src/main/java/com/jayden/notebook/NoteViewFragment.java
+++ app/src/main/java/com/jayden/notebook/NoteViewFragment.java
@@ -1,11 +1,16 @@
 package com.jayden.notebook;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -22,8 +27,21 @@ public class NoteViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_view, container, false);
+        View fragmentLayout = inflater.inflate(R.layout.fragment_note_view, container, false);
+
+        TextView title = (TextView) fragmentLayout.findViewById(R.id.viewNoteTitle);
+        TextView message = (TextView) fragmentLayout.findViewById(R.id.viewNoteMessage);
+        ImageView icon = (ImageView) fragmentLayout.findViewById(R.id.viewNoteIcon);
+
+        Intent intent = getActivity().getIntent();
+
+        title.setText(intent.getExtras().getString(MainActivity.NOTE_TITLE_EXTRA));
+        message.setText(intent.getExtras().getString(MainActivity.NOTE_MESSAGE_EXTRA));
+
+        Note.Category noteCat = (Note.Category) intent.getSerializableExtra(MainActivity.NOTE_CATEGORY_EXTRA);
+        icon.setImageResource(Note.categoryToDrawable(noteCat));
+
+        return fragmentLayout;
     }
 
 }
