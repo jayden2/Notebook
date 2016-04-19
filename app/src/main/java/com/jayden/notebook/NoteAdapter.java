731f1ec9a06b9de58f5ebb304b4eb5edfd8a0773
Jayden Spencer <jaydenspencer1@gmail.com>
2016-04-19 15:07:04 +0800

    Optimised note adapter with ViewHolder class
    using ViewHolder to get/set data

--- app/src/main/java/com/jayden/notebook/NoteAdapter.java
+++ app/src/main/java/com/jayden/notebook/NoteAdapter.java
@@ -8,12 +8,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 /**
  * Created by Jayden on 19-Apr-16.
  */
 public class NoteAdapter extends ArrayAdapter<Note> {
+
+    public static class ViewHolder {
+        TextView noteTitle;
+        TextView noteText;
+        ImageView noteIcon;
+    }
 
     public NoteAdapter(Context context, ArrayList<Note> notes) {
         super(context, 0, notes);
@@ -24,20 +32,36 @@ public class NoteAdapter extends ArrayAdapter<Note> {
         //Get the data item for this position
         Note note = getItem(position);
 
+        //Create a new viewHolder
+        ViewHolder viewHolder;
+
         //Check if an existing view is being reused, otherwise inflate a new view from custom row layout
         if (convertView == null) {
+
+            //if there's no view being used create one, and create
+            //a view holder along with it to save the view reference to it
+            viewHolder = new ViewHolder();
+
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_row, parent, false);
+
+            //Grab references of views so we can populate them with specific note row data
+            //Set view to view holder
+            viewHolder.noteTitle = (TextView) convertView.findViewById(R.id.listItemNoteTitle);
+            viewHolder.noteText = (TextView) convertView.findViewById(R.id.listItemNoteBody);
+            viewHolder.noteIcon = (ImageView) convertView.findViewById(R.id.listItemNoteImg);
+
+            //User set tag to store the view holder which is holding reference to the widgets
+            convertView.setTag(viewHolder);
+        } else {
+
+            //Grab widgets from view holder
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        //Grab references of views so we can populate them with specific note row data
-        TextView noteTitle = (TextView) convertView.findViewById(R.id.listItemNoteTitle);
-        TextView noteText = (TextView) convertView.findViewById(R.id.listItemNoteBody);
-        ImageView noteIcon = (ImageView) convertView.findViewById(R.id.listItemNoteImg);
-
-        //Fill each new referenced view with data associated with note it's referencing
-        noteTitle.setText(note.getTitle());
-        noteText.setText(note.getMessage());
-        noteIcon.setImageResource(note.getAssociatedDrawable());
+        //Populate date into the template view using the data object
+        viewHolder.noteTitle.setText(note.getTitle());
+        viewHolder.noteText.setText(note.getMessage());
+        viewHolder.noteIcon.setImageResource(note.getAssociatedDrawable());
 
         return convertView;
     }
